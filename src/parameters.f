eb81fc14a85e6c10d4ab879c1b68149941d7f722
toya42 <51504782+toya42@users.noreply.github.com>
2022-12-17 02:45:32 +0000

    Deploying to gh-pages from @ toya42/dmr@b3da201c31a3020df0bda26929999ffba714f9cd 🚀

--- src/parameters.f
+++ src/parameters.f
@@ -1,5 +1,11 @@
       implicit double precision (a-h,o-z)
-      parameter(jmax=1001,kmax=251)
+      !> x方向格子点数
+      parameter(jmax=1001)
+      !> y方向格子点数
+      parameter(kmax= 251)
+      !> 時間刻み幅
       parameter(dt=5.0d-5)
+      !> 比熱比と関連定数
       parameter(gmma = 1.4d0, gmm1 = gmma-1.0d0, gm1i = 1.0d0/gmm1)
+      !> 定数（計算速度性能に寄与するかはよくわからない）
       parameter(onethird=1.0d0/3.0d0)
